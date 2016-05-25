a127d54d6a6e5ecd752943c3d83ca2026fe6a0f2
Nicolas Batara <brassplayer89@gmail.com>
2016-05-25 12:12:29 -0700

    formatting changes, commented some code potions out

--- lmg.py
+++ lmg.py
@@ -1,82 +1,84 @@
 # Light Mediated Growth Simulation Software
 # Main File
 
+import numpy
+import scipy.io
+import h5py
+import sys
+import time
 
-import numpy, scipy.io, h5py , sys, time
 
-def openMatFile(fileName):
+def open_mat_file(file_name):
     """Open matlab (.mat) file.
     Returns dict if version < 7.3 or HDF5 file if version == 7.3"""
     try:
-        return scipy.io.loadmat(fileName)
+        return scipy.io.loadmat(file_name)
     except NotImplementedError:
-        return h5py.File(fileName)
+        return h5py.File(file_name)
 
-def circShift(numpyArray,shiftCoord):
-    """ circShift(numpyArray,shiftCoord)..
+
+def circ_shift(numpy_array, shift_coord):
+    """ circ_shift(numpy_array,shift_coord)..
     Shift contents of numpy array circularly. Returns array of same type"""
-    shiftCoord=numpy.array(shiftCoord)*-1
+    shift_coord = numpy.array(shift_coord)*-1
     
-    if len(shiftCoord)==2:
-        return numpy.concatenate((numpy.concatenate((numpyArray[shiftCoord[0]: , shiftCoord[1]:] , numpyArray[shiftCoord[0]: , :shiftCoord[1]]),1),numpy.concatenate((numpyArray[:shiftCoord[0] , shiftCoord[1]:] , numpyArray[:shiftCoord[0] , :shiftCoord[1]]),1)),0)
+    if len(shift_coord) == 2:
+        return numpy.concatenate((numpy.concatenate((numpy_array[shift_coord[0]:, shift_coord[1]:], numpy_array[shift_coord[0]:, :shift_coord[1]]), 1), numpy.concatenate((numpy_array[:shift_coord[0], shift_coord[1]:], numpy_array[:shift_coord[0], :shift_coord[1]]), 1)), 0)
     
-    if len(shiftCoord)==3:
-        a=numpy.concatenate((numpyArray[shiftCoord[0]: , shiftCoord[1]: , shiftCoord[2]:] , numpyArray[shiftCoord[0]: , shiftCoord[1]: , :shiftCoord[2]]),2)
-        b=numpy.concatenate((numpyArray[shiftCoord[0]: , :shiftCoord[1] , shiftCoord[2]:] , numpyArray[shiftCoord[0]: , :shiftCoord[1] , :shiftCoord[2]]),2)
-        c=numpy.concatenate((a,b),1)
-        d=numpy.concatenate((numpyArray[:shiftCoord[0] , shiftCoord[1]: , shiftCoord[2]:] , numpyArray[:shiftCoord[0] , shiftCoord[1]: , :shiftCoord[2]]),2)
-        e=numpy.concatenate((numpyArray[:shiftCoord[0] , :shiftCoord[1] , shiftCoord[2]:] , numpyArray[:shiftCoord[0] , :shiftCoord[1] , :shiftCoord[2]]),2)
-        f=numpy.concatenate((d,e),1)
-        h=numpy.concatenate((c,f),0)
+    if len(shift_coord) == 3:
+        a = numpy.concatenate((numpy_array[shift_coord[0]: , shift_coord[1]: , shift_coord[2]:] , numpy_array[shift_coord[0]: , shift_coord[1]: , :shift_coord[2]]),2)
+        b = numpy.concatenate((numpy_array[shift_coord[0]: , :shift_coord[1] , shift_coord[2]:] , numpy_array[shift_coord[0]: , :shift_coord[1] , :shift_coord[2]]),2)
+        c = numpy.concatenate((a, b), 1)
+        d = numpy.concatenate((numpy_array[:shift_coord[0] , shift_coord[1]: , shift_coord[2]:] , numpy_array[:shift_coord[0] , shift_coord[1]: , :shift_coord[2]]),2)
+        e = numpy.concatenate((numpy_array[:shift_coord[0] , :shift_coord[1] , shift_coord[2]:] , numpy_array[:shift_coord[0] , :shift_coord[1] , :shift_coord[2]]),2)
+        f = numpy.concatenate((d, e), 1)
+        h = numpy.concatenate((c, f), 0)
         return h
-    
-def sumArrayNeighbors(numpyArray,neighborCoordinates):
-    """ sumArrayNeighbors(numpyArray,neighborCoordinates):
-    Sums the values of an array at relative positions specified in neighborCoordinates. Returns uint8 array unless    """
-    neighborCoordinates=numpy.array(neighborCoordinates)
-    output=numpy.zeros(numpyArray.shape,'int8')
-    for subCoord in neighborCoordinates:
-        output=output+circShift(numpyArray,subCoord)
+
+
+def sum_array_neighbors(numpy_array, neighbor_coordinates):
+    """ sum_array_neighbors(numpy_array,neighbor_coordinates):
+    Sums the values of an array at relative positions specified in neighbor_coordinates."""
+    neighbor_coordinates = numpy.array(neighbor_coordinates)
+    output = numpy.zeros(numpy_array.shape, 'int8')
+    for subCoord in neighbor_coordinates:
+        output = output+circ_shift(numpy_array, subCoord)
     return output
-        
-
-## Test CircShift        
-
-#arraySize=int(1e2)        
-#a=numpy.random.randint(2,size=(arraySize,arraySize,arraySize))
-#a=numpy.array(a,'uint8')
-
-#time1=time.time()
-#for x in xrange(1):
-    #b1=numpy.roll(numpy.roll(numpy.roll(a,3,0),3,1),-2,2)
-#print  str(time.time()-time1) + 's to complete roll'
-
-#time2=time.time()
-#for x in xrange(1):
-    #b2=circShift(a,[3,3,-2])
-
-#print  str(time.time()-time2) + 's to complete circshift'
 
 
+# Test CircShift
+
+# arraySize=int(1e2)
+# a=numpy.random.randint(2,size=(arraySize,arraySize,arraySize))
+# a=numpy.array(a,'uint8')
+
+# time1=time.time()
+# for x in xrange(1):
+    # b1=numpy.roll(numpy.roll(numpy.roll(a,3,0),3,1),-2,2)
+# print  str(time.time()-time1) + 's to complete roll'
+
+# time2=time.time()
+# for x in xrange(1):
+    # b2=circ_shift(a,[3,3,-2])
+
+# print  str(time.time()-time2) + 's to complete circshift'
+
+# a=open_mat_file('iteration30.mat')
+# a=open_mat_file('setup_parameters.mat')
 
 
-
-# a=openMatFile('iteration30.mat')
-#a=openMatFile('setup_parameters.mat')
-
-
-#for key in a.keys():
-    #print key
-    #print(a[key])
+# for key in a.keys():
+    # print key
+    # print(a[key])
     
 
-###Main Execution Block
+# Main Execution Block
 
 # Load setup parameters
 
 # Load other variables
 
-## Loop over iteration numbers
+# Loop over iteration numbers
 
 # Identify Iteration
 
@@ -86,8 +88,8 @@ def sumArrayNeighbors(numpyArray,neighborCoordinates):
 
 # Write Structure
 
-## End Loop
+# End Loop
 
 
 print('Completed Successfully!')
-#exit()
+# exit()
